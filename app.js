64a78619e705dd39a9ca7738a41fee47b25b2cce
JorgeMont <monterrosaspix@gmail.com>
2021-07-01 19:50:50 -0500

    Switch implemented

--- app.js
+++ app.js
@@ -3,30 +3,35 @@ const screen = document.getElementById("screen");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    if (button.value === "del") {
-      //Borra
-      screen.value = screen.value.slice(0, -1);
-    } else if (
-      button.value === "+" ||
-      button.value === "-" ||
-      button.value === "/" ||
-      button.value === "x"
-    ) {
-      //Operaciones a pantalla
-      screen.value = String(screen.value) + String(button.value);
-    } else if (button.value === "reset") {
-      // Borra todo
-      screen.value = "";
-    } else if (button.value === "=") {
-      screen.value = screen.value.replace("x", "*");
-      try {
-        // Evalua string como operacion
-        screen.value = eval(screen.value);
-      } catch {
-        screen.value = "Invalid operation";
-      }
-    } else {
-      screen.value = String(screen.value) + String(button.value);
+    switch (button.value) {
+      case "del":
+        // Deletes value
+        screen.value = screen.value.slice(0, -1);
+        break;
+      case "+":
+      case "-":
+      case "/":
+      case "x":
+        //Operators to display
+        screen.value = String(screen.value) + String(button.value);
+        break;
+      case "reset":
+        // Erases screen
+        screen.value = "";
+        break;
+      case "=":
+        // Displays result
+        screen.value = screen.value.replace("x", "*");
+        try {
+          // Evaluates string as operation
+          screen.value = eval(screen.value);
+        } catch {
+          screen.value = "Invalid operation";
+        }
+        break;
+      default:
+        //Gets numbers
+        screen.value = String(screen.value) + String(button.value);
     }
   });
 });
